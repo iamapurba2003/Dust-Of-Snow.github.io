a727f1f7f8020fa7370ddfb25e4ba5a34b6b7e63
Apurba <new.to.world2003@gmail.com>
2021-02-11 23:16:45 +0530

    Added a test file to check the github site

---
+++ Sample.py
@@ -0,0 +1,24 @@
+import random
+var = random.randint(0,100)
+print(f"Hello I am Python Interpreter and this time my random choice using the random module is {var}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
